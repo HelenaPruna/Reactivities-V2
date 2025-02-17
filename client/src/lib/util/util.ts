4f523d4ef8ec4f1a5bd0b4e6791694ab385e2e53
HelenaPruna <72511529+HelenaPruna@users.noreply.github.com>
2025-02-17 13:19:30 +0100

    End section 9 w/o counter

--- client/src/lib/util/util.ts
+++ client/src/lib/util/util.ts
@@ -1,5 +1,7 @@
 import { DateArg, format } from "date-fns";
+import {ca} from "date-fns/locale";
+
 
 export function formatDate(date: DateArg<Date>) {
-    return format(date, 'dd MMM yyyy HH:mm');
+    return format(date, "dd MMMMMM yyyy, HH:mm", {locale: ca});
 }
